f9d0c817337cdcaa56e616155ed1d98b050bd8f1
cvClaudiu <xenon@LAPTOP-LQVONC54>
2025-08-15 21:02:03 +0300

    Update project with a minor change for the sake of testing

--- converter_frontend/src/components/ChooseFile.jsx
+++ converter_frontend/src/components/ChooseFile.jsx
@@ -1,6 +1,6 @@
 const ChooseFile = ({ file, onFileChange }) => {
     return (
-        <div style={{ marginBottom: 12 }}>
+        <div style={{ marginBottom: 120 }}>
             <label style={{ display: 'block', marginBottom: 6 }}>Choose file</label>
             <input type="file" accept="audio/*" onChange={onFileChange} />
         </div>
